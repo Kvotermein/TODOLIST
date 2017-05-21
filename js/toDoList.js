95d4cebeebabe4129c8f6407ec331530a2d0ae33
Pavel <pavelblr02@mail.ru>
2017-05-21 13:33:43 +0400

    Last update

--- js/toDoList.js
+++ js/toDoList.js
@@ -1,5 +1,22 @@
 'use strict'
 let TasksArray=[];
+function loadBitch() {
+  if (TasksArray.length!==0) {
+    for (var i = 0; i < TasksArray.length; i++) {
+      TasksArray[i]
+      var li = document.createElement("li");;
+      var t = document.createTextNode(TasksArray[i]);
+      li.appendChild(t);
+      document.getElementById("myUL").insertBefore(li,uLlist.firstChild);
+      var span = document.createElement("SPAN");
+      var txt = document.createTextNode("\u00D7");
+      span.className = "close";
+      span.appendChild(txt);
+      li.appendChild(span);
+      console.log(TasksArray)     
+    }
+  }
+}
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 for (var i = 0; i < myNodelist.length; i++) {
@@ -8,6 +25,11 @@ for (var i = 0; i < myNodelist.length; i++) {
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
+  var textInTastk=myNodelist[i].textContent
+  var textInTastklength=textInTastk.length
+  var x=textInTastk.substring(0, textInTastklength - 1)
+  TasksArray.push(x)
+  console.log(TasksArray)
 }
 
 // Click on a close button to hide the current list item
@@ -16,7 +38,10 @@ var uLlist= document.getElementById("myUL")
 for (var i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
-    uLlist.removeChild(div)
+    uLlist.removeChild(div);
+    var a=TasksArray.indexOf(div.textContent);
+    TasksArray.splice(a,1);
+    console.log(TasksArray)
   }
 }
 
@@ -33,7 +58,6 @@ function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("userTask").value;
   var t = document.createTextNode(inputValue);
-  TasksArray.push(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
     alert("You must write something!");
@@ -41,20 +65,45 @@ function newElement() {
     document.getElementById("myUL").insertBefore(li,uLlist.firstChild);
   }
   document.getElementById("userTask").value = "";
-  console.log(TasksArray)
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
+  var textInTastk=li.textContent
+  var textInTastklength=textInTastk.length
+  var x=textInTastk.substring(0, textInTastklength - 1)
+  TasksArray.push(x)
+  // TasksArray.push(li.textContent);
+  console.log(TasksArray)
 
   for (var i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       var div = this.parentElement;
       uLlist.removeChild(div)
+      var a=TasksArray.indexOf(div.textContent);
+      TasksArray.splice(a,1);
+      console.log(TasksArray)
     }
   }
 }
+function ready() {
+
+  var getcont=window.localStorage.getItem('lsName');
+    if ( getcont ) {
+    var inarr=JSON.parse(getcont)
+     TasksArray=inarr;
+     console.log(inarr);
+    }
+    loadBitch() 
+  }
+
+  document.addEventListener("DOMContentLoaded", ready);
+
+window.onbeforeunload = function() {
+    var arr=JSON.stringify(TasksArray);
+        window.localStorage.setItem('lsName',arr);
+};
 
 // function start() {
 // var StoredName=window.localStorage.getItem('lsName');
